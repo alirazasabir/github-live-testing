3ed4c4553c7f2f9372d48f101203636dc08bbc1f
alirazasabir <alirazasabir94@gmail.com>
2020-08-25 09:27:42 +0500

    Updates

---
+++ precache-manifest.98eb5917e5375c04ffad11928e245363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c232a5d08b6236d8cadd69566c3cc519",
+    "url": "/github-live-testing/index.html"
+  },
+  {
+    "revision": "514b024720fbaf2126cb",
+    "url": "/github-live-testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67e3777a8a664264db96",
+    "url": "/github-live-testing/static/js/2.e7a556fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-live-testing/static/js/2.e7a556fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514b024720fbaf2126cb",
+    "url": "/github-live-testing/static/js/main.616cd123.chunk.js"
+  },
+  {
+    "revision": "754a53ead9e5d8aaf215",
+    "url": "/github-live-testing/static/js/runtime-main.5735c665.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-live-testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
